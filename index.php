34f9f49dbd07b0b7b44a748fedd26d40d177de60
Forsvunnet <eivin@forsvunnet.co.uk>
2013-01-24 18:29:40 +0100

    Testing git pull

--- index.php
+++ index.php
@@ -3,12 +3,13 @@ $github_ips = explode(', ', '207.97.227.253, 50.57.128.197, 108.171.174.178, 50.
 
 $log = json_decode(file_get_contents('log.txt'));
 if (isset($_POST) && count($_POST) > 0) {
-  if (!empty($log)) {
+  /*if (!empty($log)) {
     $log [] = $_POST;
   } else {
-    $log = array($_POST);
-  }
+  }*/
+  $log = array('git'=>shell_exec('git pull'),'post'=>$_POST);
   file_put_contents('log.txt', json_encode($log));
+
 } else {
   echo 
       "<pre>"
