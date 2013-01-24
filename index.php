ea688be94ed3bb85e3d7719608d8e2132c4c330b
Forsvunnet <eivin@forsvunnet.co.uk>
2013-01-24 19:41:51 +0100

    Ip comparison

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
  <?php 
-$github_ips = explode(', ', '207.97.227.253, 50.57.128.197, 108.171.174.178, 50.57.231.61');
+$github_ips = '207.97.227.253, 50.57.128.197, 108.171.174.178, 50.57.231.61';
 
 $log = json_decode(file_get_contents('log.txt'));
 if (isset($_POST) && count($_POST) > 0) {
@@ -11,9 +11,7 @@ if (isset($_POST) && count($_POST) > 0) {
   file_put_contents('log.txt', json_encode($log));
 
 } else {
-  echo 
-      "<pre>"
-    . var_dump($log, $github_ips)
-    . "</pre>"
+  echo
+    $log . '<hr>' . $github_ips
     ;
 }
